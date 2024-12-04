c4ad3b1ec72127085c96d8b4765fb27c4d88572d
nitehawk <nitehawk@nitespace.co>
2024-12-04 15:56:41 -0500

    Magic regex games.

--- day3/day3b.go
+++ day3/day3b.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
 func day3b() int {
-	return 0
+	sum := 0
+	ena := true
+
+	fmt.Println("Compiling search for mul()")
+	re := regexp.MustCompile(`(don)\'t\(\)|(do)\(\)|mul\(([0-9]+),([0-9]+)\)`)
+
+	fmt.Println("Opening memory")
+	scan := day3readInput("input.txt")
+
+	fmt.Println("Reading memory")
+	for scan.Scan() {
+		line := scan.Text()
+		matches := re.FindAllStringSubmatch(line, -1)
+		for i := 0; i < len(matches); i++ {
+			cmd := matches[i]
+			// Filter do/don't commands
+			if cmd[1] == "don" {
+				ena = false
+				continue
+			}
+			if cmd[2] == "do" {
+				ena = true
+				continue
+			}
+			if ena {
+				a, _ := strconv.Atoi(cmd[3])
+				b, _ := strconv.Atoi(cmd[4])
+				sum += a * b
+			}
+		}
+	}
+
+	return sum
 }
