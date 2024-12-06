cdfbd9c944267eebccd3d8ece8f928f794fa88dd
nitehawk <nitehawk@nitespace.co>
2024-12-06 08:33:40 -0500

    Avoid unused variables w/ input files

--- day6/run.go
+++ day6/run.go
@@ -6,9 +6,8 @@ import (
 
 func main() {
 	day := 6
-	inP := "input.txt"
-	inT := "inputtest.txt"
+	inputs := []string{"input.txt", "inputtest.txt"}
 
-	fmt.Printf("Day %d, puzzle a: %d\n", day, puzzlea(inP))
-	fmt.Printf("Day %d, puzzle b: %d\n", day, puzzleb(inT))
+	fmt.Printf("Day %d, puzzle a: %d\n", day, puzzlea(inputs[0]))
+	fmt.Printf("Day %d, puzzle b: %d\n", day, puzzleb(inputs[1]))
 }
--- template/run.go
+++ template/run.go
@@ -4,8 +4,7 @@ import "fmt"
 
 func main() {
 	day := 0
-	inP := "input.txt"
-	inT := "inputtest.txt"
-	fmt.Printf("Day %d, puzzle a: %d\n", day, puzzlea(inP))
-	fmt.Printf("Day %d, puzzle b: %d\n", day, puzzleb(inT))
+	inputs := []string{"input.txt", "inputtest.txt"}
+	fmt.Printf("Day %d, puzzle a: %d\n", day, puzzlea(inputs[1]))
+	fmt.Printf("Day %d, puzzle b: %d\n", day, puzzleb(inputs[1]))
 }
