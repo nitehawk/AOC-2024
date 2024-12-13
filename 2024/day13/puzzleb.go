5f73f771210d9bcc31b9e5351edc8d6d74633171
nitehawk <nitehawk@nitespace.co>
2024-12-13 00:39:41 -0500

    A bit of math never hurt, right?

---
+++ 2024/day13/puzzleb.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"regexp"
+	"strconv"
+
+	"github.com/nitehawk/advent-of-code/aoclib"
+)
+
+func winClawMachineB(machine []string, offset int) int {
+	reBut := regexp.MustCompile(`Button (A|B): X\+([0-9]+), Y\+([0-9]+)`)
+	rePrize := regexp.MustCompile(`Prize: X=([0-9]+), Y=([0-9]+)`)
+
+	buttonA := reBut.FindStringSubmatch(machine[0])
+	buttonB := reBut.FindStringSubmatch(machine[1])
+	prize := rePrize.FindStringSubmatch(machine[2])
+
+	buttonAX, _ := strconv.Atoi(buttonA[2])
+	buttonAY, _ := strconv.Atoi(buttonA[3])
+	buttonBX, _ := strconv.Atoi(buttonB[2])
+	buttonBY, _ := strconv.Atoi(buttonB[3])
+	prizeX, _ := strconv.Atoi(prize[1])
+	prizeY, _ := strconv.Atoi(prize[2])
+
+	prizeX += offset
+	prizeY += offset
+
+	//fmt.Println(buttonAX, buttonAY, buttonBX, buttonBY, prizeX, prizeY)
+	// Solve for ka & kb
+	// px = ka*ax + kb*bx
+	// py = ka*ay + kb*by
+	// kb = (pxay - pyax) / (bxay - byax)
+	// ka = (px - kb*bx) / ax
+
+	kb := (prizeX*buttonAY - prizeY*buttonAX) / (buttonBX*buttonAY - buttonAX*buttonBY)
+	ka := (prizeX - kb*buttonBX) / buttonAX
+
+	// Verify the solution
+	if ka*buttonAX+kb*buttonBX != prizeX {
+		return 0
+	}
+	if ka*buttonAY+kb*buttonBY != prizeY {
+		return 0
+	}
+
+	cost := ka*3 + kb
+	return cost
+}
+
+func puzzleb(inF string) int {
+	clawpuz := aoclib.ReadStringSlice(inF)
+
+	offset := 10000000000000
+
+	totalTokens := 0
+	// each machine is 3 lines long followed by a blank line
+	for i := 0; i < len(clawpuz); i += 4 {
+		machine := clawpuz[i : i+3]
+		win := winClawMachineB(machine, offset)
+		totalTokens += win
+	}
+
+	return totalTokens
+}
