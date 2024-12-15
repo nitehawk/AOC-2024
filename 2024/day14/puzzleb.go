43b1b6dce3750cb1a47d34ec440b5e90087031ed
nitehawk <nitehawk@nitespace.co>
2024-12-14 21:03:35 -0500

    build some framework - still pondering how to identify the egg...

--- 2024/day14/puzzleb.go
+++ 2024/day14/puzzleb.go
@@ -1,5 +1,40 @@
 package main
 
+import "github.com/nitehawk/advent-of-code/aoclib"
+
+func botTimeStep(bot robo) robo {
+	bot.ex += bot.vx
+	bot.ey += bot.vy
+
+	// Wrap around
+	if bot.ex < 0 {
+		bot.ex += spacew
+	} else if bot.ex >= spacew {
+		bot.ex -= spacew
+	}
+	if bot.ey < 0 {
+		bot.ey += spaceh
+	} else if bot.ey >= spaceh {
+		bot.ey -= spaceh
+	}
+	return bot
+}
+
+func botRenderState(bots []robo) {
+}
+
+func botEggSearch(bots []robo) {
+	for b := 0; b < len(bots); b++ {
+		bots[b] = botTimeStep(bots[b])
+	}
+	botRenderState(bots)
+}
+
 func puzzleb(inF string) int {
+	botdesc := aoclib.ReadStringSlice(inF)
+	bots := parseBots(botdesc)
+
+	botEggSearch(bots)
+
 	return 0
 }
